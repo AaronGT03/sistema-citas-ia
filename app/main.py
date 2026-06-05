05ba3c624c8492b8dc1781671db50bcc45a295a5
AaronGT03 <aarongalarza2000@gmail.com>
2026-06-05 17:41:17 -0600

    commit FastAPI

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine, Base
 from app.routes.citas import router as citas_router
@@ -10,6 +11,16 @@ from app.routes.usuarios import router as usuarios_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(citas_router)
 app.include_router(llamadas_router)
