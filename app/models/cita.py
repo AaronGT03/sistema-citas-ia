8be675be7e76f3c662e817373875f0ce73563ea1
ivanmonjim <ivanmonjim@gmail.com>
2026-06-13 02:11:33 -0600

    update "Crear Cita en Front"
    se hizo el crear citas en front

--- app/models/cita.py
+++ app/models/cita.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from datetime import datetime
 from app.database import Base
+from fastapi import APIRouter, Depends, HTTPException
 
 
 class Cita(Base):
